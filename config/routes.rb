ab95e2c93a08e603d4f31c48e9933448ec440703
enriquebatoon <enriquebatoon@gmail.com>
2013-04-30 15:45:08 +0800

    change route

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Suroy2::Application.routes.draw do
   ActiveAdmin.routes(self)
 
   devise_for :admin_users, ActiveAdmin::Devise.config
+  root :to => 'pages#index'
 
   get "pages/view_all"
   get "members/login"
@@ -68,7 +69,7 @@ Suroy2::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => 'welcome#index'
-  root :to => 'pages#index'
+  
   # See how all your routes lay out with "rake routes"
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
